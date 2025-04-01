b9d7d914c8f914d59f6422a7f7fb1ec061f44d19
max <2337833033@qq.com>
2025-04-01 13:18:06 +0800

    新增app初始化markdown.css主题Link

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,20 @@
     </div>
 </template>
 <script setup>
-import { ref, onMounted, defineProps, defineEmits, defineExpose } from 'vue'
-const emits = defineEmits([''])
-const props = defineProps({})
-onMounted(() => {})
-defineExpose({})
+import { ref, onMounted, defineProps } from 'vue';
+import useTheme from '@/hooks/useTheme';
+const { toogleLink } = useTheme();
+const props = defineProps({});
+onMounted(() => {
+    toogleLink();
+});
 </script>
 <style lang="scss">
 html {
     background-color: var(--mainBgColor);
 }
 </style>
-<style scoped lang='scss'>
+<style scoped lang="scss">
 #app {
     background-color: var(--mainBgColor);
 }
