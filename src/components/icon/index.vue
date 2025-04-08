28c2449fe522db3f25504607478c5746065ee987
max <2337833033@qq.com>
2025-04-08 17:39:15 +0800

    icon增加

--- src/components/icon/index.vue
+++ src/components/icon/index.vue
@@ -1,18 +1,13 @@
 <template>
-    <i
-        :class="['iconfont', icons[props.type], 'icon']"
-        :style="{ fontSize }"
-        @click="handleClick"
-    >
-    </i>
+    <i :class="['iconfont', icons[props.type], 'icon']" :style="{ fontSize }" @click="handleClick"> </i>
 </template>
 <script setup>
-import { ref, onMounted, defineProps, defineEmits } from 'vue'
-const emits = defineEmits(['click'])
+import { ref, onMounted, defineProps, defineEmits } from 'vue';
+const emits = defineEmits(['click']);
 
 const handleClick = () => {
-    emits('click', props.type)
-}
+    emits('click', props.type);
+};
 const props = defineProps({
     type: {
         type: String,
@@ -22,7 +17,7 @@ const props = defineProps({
         type: String,
         default: '16px',
     },
-})
+});
 const icons = ref({
     weixin: 'icon-weixin', // 微信
     qingtian: 'icon-baitian-qing', // 晴天
@@ -35,9 +30,13 @@ const icons = ref({
     bottomArrow: 'icon-xiangxia2', //
     version: 'icon-banben', // 版本
     code: 'icon-code', // 代码
-})
+    success: 'icon-success_fill', // 成功
+    error: 'icon-error', // 错误
+    info: 'icon-info', // 信息
+    warning: 'icon-wraning', // 警告
+});
 </script>
-<style scoped lang='scss'>
+<style scoped lang="scss">
 .icon {
     cursor: pointer;
 }
