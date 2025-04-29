0cf90580117d5120b460d7803b18c4415d695317
max <2337833033@qq.com>
2025-04-29 22:18:45 +0800

    博客列表页面添加空状态与loading状态

--- src/views/blogList/index.vue
+++ src/views/blogList/index.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="blog_category_list_wrap">
+    <div class="blog_category_list_wrap" v-loading="loading">
         <div class="blog_category_list">
             <h2>Blog | 博客</h2>
-            <Article v-for="item in articleList" :key="item.id" :article="item" />
+            <div style="height: 100%">
+                <Empty v-if="articleList.length === 0" />
+                <div v-else>
+                    <Article v-for="item in articleList" :key="item.id" :article="item" />
+                </div>
+            </div>
         </div>
-        <Pager :total="total" :pageSize="limit" @pageChange="handlePageChange" />
+        <Pager :total="total" :currentPage="page" :pageSize="limit" @pageChange="handlePageChange" />
     </div>
 </template>
 <script setup>
+import Empty from '@/components/empty/index.vue';
 import Pager from '@/components/pager/index.vue';
 import Article from './components/Article.vue';
 import { ref, onMounted, getCurrentInstance } from 'vue';
@@ -16,13 +22,16 @@ const articleList = ref([]);
 const total = ref(0);
 const page = ref(1);
 const limit = ref(50);
+const loading = ref(true);
 const getArticleList = async () => {
+    loading.value = true;
     const data = { page: page.value, limit: limit.value };
     const res = await $api({ type: 'getArticle', data });
     if (res.code === 0) {
         articleList.value = res?.data?.rows ?? [];
         total.value = res?.data?.count ?? 0;
     }
+    loading.value = false;
 };
 const handlePageChange = (pageNum) => {
     page.value = pageNum;
@@ -34,6 +43,11 @@ onMounted(() => {
 });
 </script>
 <style scoped lang="scss">
+.empty_wrap {
+    width: 800px;
+    margin: 0 auto;
+    height: 100%;
+}
 .blog_category_list_wrap {
     height: calc(100vh - 68px);
     display: flex;
