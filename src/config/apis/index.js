3e7bd7a2d72374699843b77dc97fe44075b49dd6
max <2337833033@qq.com>
2025-05-02 12:37:47 +0800

    添加留言相关API配置

--- src/config/apis/index.js
+++ src/config/apis/index.js
@@ -3,10 +3,12 @@ import banner from './banner';
 import blog from './blog';
 import article from './article';
 import comment from './comment';
+import message from './message';
 export default {
     ...demos,
     ...banner,
     ...blog,
     ...article,
     ...comment,
+    ...message,
 };
---
+++ src/config/apis/message.js
@@ -0,0 +1,12 @@
+export default {
+    // 获取留言列表
+    getMessageList: {
+        url: '/api/message',
+        method: 'get',
+    },
+    // 添加留言
+    addMessage: {
+        url: '/api/message',
+        method: 'post',
+    },
+};
